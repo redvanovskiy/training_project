7ef11c033f03e9cc5d1e21cbcca25ead6352dde3
Serhii Redvanovskyi <redvanovskiy@gmail.com>
2023-06-16 02:48:23 +0300

    Update driver_extended.py

--- extensions/driver_extended.py
+++ extensions/driver_extended.py
@@ -10,18 +10,3 @@ class DriverExtended(EventFiringWebDriver):
 
     def load(self):
         self.get(self.base_url)
-
-    def login(self):
-        self.get(self.base_url + "/#/login")
-
-    def users(self):
-        self.get(self.base_url + "/#/users")
-
-    def users_settings(self):
-        self.get(self.base_url + "/#/settings")
-
-    def recording(self):
-        self.get(self.base_url + "/#/recording")
-
-    def recording_settings(self):
-        self.get(self.base_url + "/#/recording/settings")
