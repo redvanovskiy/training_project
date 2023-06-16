1911b88ba784ecf2d89e2ee97f69c1cbdc851598
Serhii Redvanovskyi <redvanovskiy@gmail.com>
2023-06-16 10:45:56 +0300

    Update utils.py

--- base/utils.py
+++ base/utils.py
@@ -2,8 +2,3 @@ import os
 
 # --- ENV URL ---
 HOST = os.getenv('BASE_URL', "https://www.demoblaze.com/index.html")
-
-# --- SELENIUM HUB ---
-SELENIUM_HUB_HOST = 'selenium-hub'
-SELENIUM_HUB_PORT = os.getenv('SELENIUM_HUB_PORT', '4444')
-SELENIUM_HUB_URL = 'http://' + SELENIUM_HUB_HOST + ":" + SELENIUM_HUB_PORT
