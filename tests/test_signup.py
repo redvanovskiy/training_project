2d417299ca6cb24d8431e34ba3ef7b6343a6b3d9
MaxSkydan <skidan.maxim126@gmail.com>
2023-06-28 10:41:46 +0300

    SignUpPage

--- tests/test_signup.py
+++ tests/test_signup.py
@@ -24,14 +24,14 @@ class TestSignup(BaseClass):
     fail_msg = 'Please fill out Username and Password.'
     user_exist = 'This user already exist.'
     sign_up_ok = 'Sign up successful.'
-    user_name = ''.join(random.choice(string.ascii_letters) for i in range(10))
+    cred = ''.join(random.choice(string.ascii_letters) for i in range(10))
 
     def setup_method(self):
         # Refresh page before new test
         self.page.refresh()
 
     def test_successful_signup(self):
-        self.page._signup(self.user_name, self.user_name)
+        self.page._signup(self.cred, self.cred)
         self.login.assert_text_in_alert(self.sign_up_ok)
         self.login.accept_alert()
 
@@ -46,12 +46,12 @@ class TestSignup(BaseClass):
         self.login.accept_alert()
 
     def test_failed_signup_with_empty_username(self):
-        self.page._signup('', '1234567890')
+        self.page._signup('', self.cred)
         self.login.assert_text_in_alert(self.fail_msg)
         self.login.accept_alert()
 
     def test_failed_signup_with_empty_password(self):
-        self.page._signup(self.user_name, '')
+        self.page._signup(self.cred, '')
         self.login.assert_text_in_alert(self.fail_msg)
         self.login.accept_alert()
 
@@ -60,11 +60,12 @@ class TestSignup(BaseClass):
         self.page.driver.delete_all_cookies()
         self.page.refresh()
         # Run test
-        self.page._signup(self.user_name, self.user_name)
+        loc_cred = ''.join(random.choice(string.ascii_letters) for i in range(10))
+        self.page._signup(loc_cred, loc_cred)
         self.login.assert_text_in_alert(self.sign_up_ok)
         self.login.accept_alert()
         self.page.refresh()
-        self.login._login(self.user_name, self.user_name)
+        self.login._login(loc_cred, loc_cred)
         self.page.visible(self.login._LOGOUT_BUTTON)
         self.login.click_logout_button()
         self.page.visible(self.login._LOGIN_BUTTON)
